cd3aaf69feb5f7e1d507030412ad03c1611e64f1
rrzt <124551348+h465855hgg@users.noreply.github.com>
2026-02-06 15:34:46 +0800

    更新了版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.web.webide"
         minSdk = 29
         targetSdk = 36
-        versionCode = 23
-        versionName = "0.2.3"
+        versionCode = 24
+        versionName = "0.2.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             //noinspection ChromeOsAbiSupport
@@ -63,7 +63,7 @@ android {
 
         release {
            // applicationIdSuffix = ".release"
-            versionNameSuffix = "-release-Preview"
+            versionNameSuffix = "-release"//-Preview
 
             isMinifyEnabled = true
             isShrinkResources = true // 资源缩减
