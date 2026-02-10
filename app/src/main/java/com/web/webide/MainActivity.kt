a4c83f98e175e9c96032b3baeb508f50c43784d6
rrzt <124551348+h465855hgg@users.noreply.github.com>
2026-02-10 22:07:10 +0800

    refactor(app): Revert MainActivity to ComponentActivity

--- app/src/main/java/com/web/webide/MainActivity.kt
+++ app/src/main/java/com/web/webide/MainActivity.kt
@@ -20,7 +20,7 @@
 package com.web.webide
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedContent
@@ -46,7 +46,7 @@ import com.web.webide.ui.editor.components.TextMateInitializer
 import com.web.webide.ui.theme.MyComposeApplicationTheme
 import com.web.webide.ui.welcome.WelcomeScreen
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
